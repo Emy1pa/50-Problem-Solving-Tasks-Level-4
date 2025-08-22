fc6eb81e11e170ffc8b771721f0afa39b40e2d47
Emy1pa <souaouti.iman@gmail.com>
2025-08-22 17:49:15 +0100

    Solve Problem 3: Implement concise C++ leap year checker using a single return statement with logical conditions.

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -9,22 +9,19 @@ int ReadYear() {
     return Year;
 }
 
-bool CheckIfLeapYear(int Year){
-    if (Year % 400 == 0)
-        return true;
-    else if (Year % 4 == 0 && Year % 100 != 0)
-        return true;
-    else
-        return false;
+
+bool CheckIfLeapYear(int Year) {
+    return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
+       
 }
 
 int main()
 {
     int Year = ReadYear();
     if (CheckIfLeapYear(Year))
-        cout << "Yes, it is a leap year \n";
+        cout << "\nYes, Year [" << Year << "] it is a leap year \n";
     else
-        cout << "No, it is not a leap year \n";
-    
+        cout << "\nNo, Year [" << Year << "] it is not a leap year \n";
+
     system("pause>0");
 }
