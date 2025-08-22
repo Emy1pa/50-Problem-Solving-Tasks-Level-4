6b2d77272888634219ce2883f3a279748d5e5917
Emy1pa <souaouti.iman@gmail.com>
2025-08-22 20:42:47 +0100

    Solve Problem 5: Implement C++ function to return correct days in a month using ternary operator and leap year check.

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -1,49 +1,70 @@
 #include <iostream>
 using namespace std;
 
-struct stYearInfo {
-    short Days = 0;
-    short Hours = 0;
-    int Minutes = 0;
-    int Seconds = 0;
-};
+//enum enMonthOfYear {January = 1, February = 2, March = 3, April = 4,
+//Mai = 5, June = 6, July = 7, August = 8,
+//September = 9, October = 10, November = 11,
+//December = 12};
 
 
-
-int ReadYear() {
-    int Year = 0;
+short ReadYear() {
+    short Year = 0;
     cout << "Please enter a year to check ? ";
     cin >> Year;
     return Year;
 }
 
+short ReadMonth() {
+    short Month = 0;
+    cout << "Please enter a month to check ? ";
+    cin >> Month;
+    return Month;
+}
 
-bool CheckIfLeapYear(int Year) {
+
+bool CheckIfLeapYear(short Year) {
     return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
        
 }
 
-stYearInfo YearInfo(int Year) {
-    stYearInfo YearInfo;
-    YearInfo.Days = CheckIfLeapYear(Year) ? 366 : 365;
-    YearInfo.Hours = YearInfo.Days * 24;
-    YearInfo.Minutes = YearInfo.Hours * 60;
-    YearInfo.Seconds = YearInfo.Minutes * 60;
-    return YearInfo;
+int NumberOfDaysInAMonth(short Year, short Month) {
+    return (Month == 2) ? (CheckIfLeapYear(Year) ? 29 : 28) :
+    (Month == 4 || Month == 6 || Month == 9 || Month == 1) ? 30 : 31;
 }
 
-void PrintResult(stYearInfo YearInfo, int Year){
-    
-    cout << "Number of Days in Year [" << Year << "] is " << YearInfo.Days << endl;
-    cout << "Number of Hours in Year [" << Year << "] is " << YearInfo.Hours << endl;
-    cout << "Number of Minutes in Year [" << Year << "] is " << YearInfo.Minutes << endl;
-    cout << "Number of Seconds in Year [" << Year << "] is " << YearInfo.Seconds << endl;
+int NumberOfHoursInAMonth(short Year, short Month) {
+    return NumberOfDaysInAMonth(Year, Month) * 24;
 }
+
+int NumberOfMinutesInAMonth(short Year, short Month) {
+    return NumberOfHoursInAMonth(Year, Month) * 60;
+}
+
+int NumberOfSecondsInAMonth(short Year, short Month) {
+    return NumberOfMinutesInAMonth(Year, Month) * 60;
+}
+
+
+
 
 int main()
 {
-    int Year = ReadYear();
-    PrintResult(YearInfo(Year), Year);
+    short Year = ReadYear();
+    cout << endl;
+
+    short Month = ReadMonth();
+
+    cout << "\nNumber of Days in Month [" << Year << "] is "
+        << NumberOfDaysInAMonth(Year, Month);
+
+    cout << "\nNumber of Hours in Month [" << Year << "] is "
+        << NumberOfHoursInAMonth(Year, Month);
+
+    cout << "\nNumber of Minutes in Month [" << Year << "] is "
+        << NumberOfMinutesInAMonth(Year, Month);
+
+    cout << "\nNumber of Seconds in Month [" << Year << "] is "
+        << NumberOfSecondsInAMonth(Year, Month);
     
     system("pause>0");
 }
