a11bcfd9284b66abf3170d9b5db5ba24153b7e18
Emy1pa <souaouti.iman@gmail.com>
2025-08-22 18:10:00 +0100

    Solve Problem 4: Implement C++ program to calculate days, hours, minutes, and seconds for any year, handling leap years dynamically.

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct stYearInfo {
+    short Days = 0;
+    short Hours = 0;
+    int Minutes = 0;
+    int Seconds = 0;
+};
+
+
 
 int ReadYear() {
     int Year = 0;
@@ -15,13 +23,27 @@ bool CheckIfLeapYear(int Year) {
        
 }
 
+stYearInfo YearInfo(int Year) {
+    stYearInfo YearInfo;
+    YearInfo.Days = CheckIfLeapYear(Year) ? 366 : 365;
+    YearInfo.Hours = YearInfo.Days * 24;
+    YearInfo.Minutes = YearInfo.Hours * 60;
+    YearInfo.Seconds = YearInfo.Minutes * 60;
+    return YearInfo;
+}
+
+void PrintResult(stYearInfo YearInfo, int Year){
+    
+    cout << "Number of Days in Year [" << Year << "] is " << YearInfo.Days << endl;
+    cout << "Number of Hours in Year [" << Year << "] is " << YearInfo.Hours << endl;
+    cout << "Number of Minutes in Year [" << Year << "] is " << YearInfo.Minutes << endl;
+    cout << "Number of Seconds in Year [" << Year << "] is " << YearInfo.Seconds << endl;
+}
+
 int main()
 {
     int Year = ReadYear();
-    if (CheckIfLeapYear(Year))
-        cout << "\nYes, Year [" << Year << "] it is a leap year \n";
-    else
-        cout << "\nNo, Year [" << Year << "] it is not a leap year \n";
-
+    PrintResult(YearInfo(Year), Year);
+    
     system("pause>0");
 }
