57d9801115a87843599da025c86a9d28243ec79a
Emy1pa <souaouti.iman@gmail.com>
2025-08-22 21:37:46 +0100

    Solve Problem 7: Implement C++ program to determine the day of the week for a given date using Zeller’s Congruence,  and printing day names.

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -21,20 +21,47 @@ short ReadMonth() {
     return Month;
 }
 
-
-bool CheckIfLeapYear(short Year) {
-    return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
-       
+short ReadDay() {
+    short Day = 0;
+    cout << "Please enter a day to check ? ";
+    cin >> Day;
+    return Day;
 }
 
-int NumberOfDaysInAMonth(short Year, short Month) {
-    if (Month < 1 || Month > 12)
-        return 0;
-    
-    int Days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-    return (Month == 2) ? (CheckIfLeapYear(Year) ? 29 : 28) :
-        Days[Month - 1];
+short DayOrder(short Year, short Month, short Day){
+    short a = (14 - Month) / 12;
+    short y = Year - a;
+    short m = Month + 12 * a - 2;
+    short d = (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+    return d;
+}
+
+string PrintDayOfWeek(short Day){
+    switch (Day)
+    {
+    case 0:
+        return "Sunday";
+        break;
+    case 1:
+        return "Monday";
+        break;
+    case 2:
+        return "Tuesday";
+        break;
+    case 3:
+        return "Wednesday";
+        break;
+    case 4:
+        return "Thursday";
+        break;
+    case 5:
+        return "Friday";
+        break;
+    case 6:
+        return "Saturday";
+        break;
+    }
 }
 
 
@@ -46,13 +73,17 @@ int main()
 {
     short Year = ReadYear();
     cout << endl;
-
     short Month = ReadMonth();
+    cout << endl;
+    short Day = ReadDay();
 
-    cout << "\nNumber of Days in Month [" << Month << "] is "
-        << NumberOfDaysInAMonth(Year, Month);
-
-   
+    short OrderDay = DayOrder(Year, Month, Day);
+    cout << endl;
+    cout << "Date          : " << Day << "/" << Month << "/" << Year << endl;
     
+    cout << "Day Order     : " << OrderDay << endl;
+    
+    cout << "Day Name      : " << PrintDayOfWeek(OrderDay) << endl;
+
     system("pause>0");
 }
