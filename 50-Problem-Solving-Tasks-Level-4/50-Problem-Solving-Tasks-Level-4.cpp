a82b1db233f0076a3664dc6e1042149c0c9d1ff9
Emy1pa <souaouti.iman@gmail.com>
2025-08-23 14:35:22 +0100

    Solve Problem 9: Implement full-year calendar display in C++ with proper month formatting

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -21,6 +21,14 @@ bool IsLeapYear(short Year){
     return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
 }
 
+string DayShortName(short
+    DayOfWeekOrder)
+{
+    string arrDayNames[7] = { "Sun","Mon","Tue","Wed","Thu","Fri","Sat" };
+    return arrDayNames[DayOfWeekOrder];
+}
+
+
 short CalculateDaysOfMonth(short Month, short Year){
     if (Month < 1 || Month > 12)
         return 0;
@@ -36,36 +44,54 @@ short DayOfWeekOrder(short Day, short Month, short Year) {
     return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
 }
 
+string MonthShortName(short MonthNumber){
+    string Months[12] = { "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep",
+    "Oct", "Nov", "Dec" };
+
+    return (Months[MonthNumber - 1]);
+}
 
 void DisplayMonth(short Month, short Year){
-    string arrMonthNames[12] = { "Jan","Feb","Mar","Apr","Mai","Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-    cout << "\n\t\t------------------------ " << arrMonthNames[Month - 1] << " ------------------------\n\n";
+    int NumberOfDays;
 
-    short DaysOfMonth = CalculateDaysOfMonth(Month, Year);
-    string arrDayNames[7] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-    cout << "\t\t";
+    int Current = DayOfWeekOrder(1, Month, Year);
 
-    for (int i = 0; i < 7; i++)
+    NumberOfDays = CalculateDaysOfMonth(Month, Year);
+
+    printf("\n _______________%s__________________\n\n",
+        MonthShortName(Month).c_str());
+
+    printf(" Sun Mon Tue Wed Thu Fri Sat\n");
+
+    int i;
+    for (i = 0; i < Current; i++)
+        printf("    ");
+    for (int j = 1; j <= NumberOfDays; j++)
     {
-        cout << arrDayNames[i] << "\t";
+        printf("%4d", j);
+        if (++i == 7) {
+            i = 0;
+            printf("\n");
+        }
     }
-    cout << "\n\t\t";
+    printf("\n ____________________________________\n");
 
-    short FirstDay = DayOfWeekOrder(1, Month, Year);
 
-    for (int i = 0; i < FirstDay; i++)
-        cout << "\t";
-    
-    for (int Day = 1; Day <= DaysOfMonth; Day++)
+}
+
+void DisplayYearCalendar(short Year){
+    printf("\n ____________________________________\n\n");
+    printf("            Calendar - %d       ", Year);
+    printf("\n ____________________________________\n");
+
+    for (int i = 1; i <= 12; i++)
     {
-        cout << Day << "\t";
-
-        if ((FirstDay + Day) % 7 == 0) 
-            cout << "\n\t\t";
+        DisplayMonth(i, Year);
     }
-    cout << "\n\t\t-----------------------------------------------------\n";
-    
+
 }
 
 
@@ -75,11 +101,9 @@ int main()
 {
     short Year = ReadYear();
     cout << endl;
-    short Month = ReadMonth();
-    cout << endl;
     
-    DisplayMonth(Month, Year);
-   
+    
+    DisplayYearCalendar(Year);
 
     system("pause>0");
 }
