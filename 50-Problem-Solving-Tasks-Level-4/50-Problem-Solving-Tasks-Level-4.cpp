faa6c102bb4a7ad409d1b77362cbaf31f50dda08
Emy1pa <souaouti.iman@gmail.com>
2025-08-23 14:01:27 +0100

    Solve Problem 8: Implement C++ program to display month calendar using leap year calculation logic

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//enum enMonthOfYear {January = 1, February = 2, March = 3, April = 4,
-//Mai = 5, June = 6, July = 7, August = 8,
-//September = 9, October = 10, November = 11,
-//December = 12};
 
 
 short ReadYear() {
@@ -21,50 +17,56 @@ short ReadMonth() {
     return Month;
 }
 
-short ReadDay() {
-    short Day = 0;
-    cout << "Please enter a day to check ? ";
-    cin >> Day;
-    return Day;
+bool IsLeapYear(short Year){
+    return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
+}
+
+short CalculateDaysOfMonth(short Month, short Year){
+    if (Month < 1 || Month > 12)
+        return 0;
+    short NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+short DayOfWeekOrder(short Day, short Month, short Year) {
+    short a, y, m;
+    a = (14 - Month) / 12;
+    y = Year - a;
+    m = Month + (12 * a) - 2;
+    return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
 }
 
 
-short DayOrder(short Year, short Month, short Day){
-    short a = (14 - Month) / 12;
-    short y = Year - a;
-    short m = Month + 12 * a - 2;
-    short d = (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
-    return d;
-}
+void DisplayMonth(short Month, short Year){
+    string arrMonthNames[12] = { "Jan","Feb","Mar","Apr","Mai","Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-string PrintDayOfWeek(short Day){
-    switch (Day)
+    cout << "\n\t\t------------------------ " << arrMonthNames[Month - 1] << " ------------------------\n\n";
+
+    short DaysOfMonth = CalculateDaysOfMonth(Month, Year);
+    string arrDayNames[7] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+    cout << "\t\t";
+
+    for (int i = 0; i < 7; i++)
     {
-    case 0:
-        return "Sunday";
-        break;
-    case 1:
-        return "Monday";
-        break;
-    case 2:
-        return "Tuesday";
-        break;
-    case 3:
-        return "Wednesday";
-        break;
-    case 4:
-        return "Thursday";
-        break;
-    case 5:
-        return "Friday";
-        break;
-    case 6:
-        return "Saturday";
-        break;
+        cout << arrDayNames[i] << "\t";
     }
+    cout << "\n\t\t";
+
+    short FirstDay = DayOfWeekOrder(1, Month, Year);
+
+    for (int i = 0; i < FirstDay; i++)
+        cout << "\t";
+    
+    for (int Day = 1; Day <= DaysOfMonth; Day++)
+    {
+        cout << Day << "\t";
+
+        if ((FirstDay + Day) % 7 == 0) 
+            cout << "\n\t\t";
+    }
+    cout << "\n\t\t-----------------------------------------------------\n";
+    
 }
-
-
 
 
 
@@ -75,15 +77,9 @@ int main()
     cout << endl;
     short Month = ReadMonth();
     cout << endl;
-    short Day = ReadDay();
-
-    short OrderDay = DayOrder(Year, Month, Day);
-    cout << endl;
-    cout << "Date          : " << Day << "/" << Month << "/" << Year << endl;
     
-    cout << "Day Order     : " << OrderDay << endl;
-    
-    cout << "Day Name      : " << PrintDayOfWeek(OrderDay) << endl;
+    DisplayMonth(Month, Year);
+   
 
     system("pause>0");
 }
