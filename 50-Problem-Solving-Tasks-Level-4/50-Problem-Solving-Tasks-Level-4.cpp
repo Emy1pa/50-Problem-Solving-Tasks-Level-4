7a5960d17ce9ffb232326e252cde406b751bb9cf
Emy1pa <souaouti.iman@gmail.com>
2025-08-30 22:54:31 +0100

    Solve Problem 62: implement function to validate a given date by checking month and day limits including leap year handling

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -31,41 +31,12 @@ struct sDate {
     short Day;
 };
 
-struct stPeriod {
-    sDate StartDate;
-    sDate EndDate;
-};
-
 sDate ReadFullDate() {
     sDate Date;
     Date.Day = ReadDay();
     Date.Month = ReadMonth();
     Date.Year = ReadYear();
     return Date;
-}
-
-bool IsDate1BeforeDate2(sDate Date1, sDate Date2)
-{
-    return (Date1.Year < Date2.Year) ? true :
-        ((Date1.Year == Date2.Year) ?
-            (Date1.Month < Date2.Month ? true :
-                (Date1.Month == Date2.Month ?
-                    Date1.Day < Date2.Day : false))
-            : false);
-}
-
-bool IsDate1EqualDate2(sDate Date1, sDate Date2)
-{
-    return (Date1.Year == Date2.Year) ? ((
-        Date1.Month == Date2.Month) ?
-        ((Date1.Day == Date2.Day) ? true :
-            false) : false) : false;
-}
-
-bool IsDate1AfterDate2(sDate Date1, sDate Date2)
-{
-    return (!IsDate1BeforeDate2(Date1, Date2)
-        && !IsDate1EqualDate2(Date1, Date2));
 }
 
 
@@ -119,137 +90,25 @@ sDate IncreaseDateByOneDay(sDate Date)
     return Date;
 }
 
-//short CalculatePeriodLength(stPeriod Period, bool IncludeEndDay= false){
-//    int Days = 0;
-//
-//    while (IsDate1BeforeDate2(Period.StartDate, Period.EndDate)) {
-//        Days++;
-//        Period.StartDate = IncreaseDateByOneDay(Period.StartDate);
-//    }
-//    return IncludeEndDay ? ++Days : Days;
-//}
 
-void SwapDates(sDate& Date1, sDate& Date2)
-{
-    sDate TempDate;
-    TempDate.Year = Date1.Year;
-    TempDate.Month = Date1.Month;
-    TempDate.Day = Date1.Day;
-    Date1.Year = Date2.Year;
-    Date1.Month = Date2.Month;
-    Date1.Day = Date2.Day;
-    Date2.Year = TempDate.Year;
-    Date2.Month = TempDate.Month;
-    Date2.Day = TempDate.Day;
-}
-
-int GetDifferenceInDays(sDate Date1, sDate Date2, bool IncludeEndDay = false)
-{
-    int Days = 0;
-    short SwapFlagValue = 1;
-    if (!IsDate1BeforeDate2(Date1, Date2))
-    {
-        SwapDates(Date1, Date2);
-        SwapFlagValue = -1;
-    }
-    while (IsDate1BeforeDate2(Date1, Date2))
-    {
-        Days++;
-        Date1 = IncreaseDateByOneDay(Date1);
-    }
-    return IncludeEndDay ? ++Days * SwapFlagValue : Days * SwapFlagValue;
-}
-
-enum enDateCompare { Before = -1, Equal = 0, After = 1 };
-
-enDateCompare CompareDates(sDate Date1, sDate Date2)
-{
-    if (IsDate1BeforeDate2(Date1, Date2))
-        return enDateCompare::Before;
-    if (IsDate1EqualDate2(Date1, Date2))
-        return enDateCompare::Equal;
-    return enDateCompare::After;
-}
-
-bool IsOverlapPeriods(stPeriod Period1, stPeriod Period2)
-{
-    if (
-        CompareDates(Period2.EndDate, Period1.StartDate) ==
-        enDateCompare::Before
-        ||
-        CompareDates(Period2.StartDate, Period1.EndDate) ==
-        enDateCompare::After
-        )
+bool CheckForValidatedDate(sDate Date){
+    
+    short NumberOfDaysInCurrentMonth = NumberOfDaysInAMonth(Date.Month, Date.Year);
+    if ((Date.Month < 1 || Date.Month > 12) || (Date.Day < 1 || Date.Day > NumberOfDaysInCurrentMonth))
         return false;
     else
         return true;
 }
 
-int PeriodLengthInDays(stPeriod Period, bool IncludeEndDate = false)
-{
-    return GetDifferenceInDays(Period.StartDate, Period.EndDate,
-        IncludeEndDate);
-}
-bool isDateInPeriod(sDate Date, stPeriod Period)
-{
-    return !(CompareDates(Date, Period.StartDate) == enDateCompare::Before
-        ||
-        CompareDates(Date, Period.EndDate) == enDateCompare::After);
-}
-
-int CountOverLapDays(stPeriod Period1, stPeriod Period2){
-    int Period1Length = PeriodLengthInDays(Period1, true);
-    int Period2Length = PeriodLengthInDays(Period2, true);
-    int OverlapDays = 0;
-
-    if (!IsOverlapPeriods(Period1, Period2))
-        return 0;
-
-    if (Period1Length < Period2Length) {
-        while (IsDate1BeforeDate2(Period1.StartDate, Period1.EndDate)) {
-            if (isDateInPeriod(Period1.StartDate, Period2))
-                OverlapDays++;
-            Period1.StartDate = IncreaseDateByOneDay(Period1.StartDate);
-        }
-    }
-    else {
-        while (IsDate1BeforeDate2(Period2.StartDate, Period2.EndDate)) {
-            if (isDateInPeriod(Period2.StartDate, Period1))
-                OverlapDays++;
-            Period2.StartDate = IncreaseDateByOneDay(Period2.StartDate);
-        }
-    }
-    return OverlapDays;
-}
-
-
 int main()
 {
+    sDate Date = ReadFullDate();
 
+    if (CheckForValidatedDate(Date))
+        cout << "\nYes, Date is a validated date.\n";
+    else
+        cout << "\nNo, Date is not a validated date.\n";
     
-    stPeriod Period1, Period2;
-
-   
-    cout << "Enter Period 1: \n";
-    cout << "Enter Start Date: \n\n";
-
-    Period1.StartDate = ReadFullDate();
-
-    cout << "\nEnter End Date: \n\n";
-
-    Period1.EndDate = ReadFullDate();
-
-    
-    cout << "\n\nEnter Period 2: \n";
-    cout << "Enter Start Date: \n\n";
-
-    Period2.StartDate = ReadFullDate();
-
-    cout << "\nEnter End Date: \n\n";
-
-    Period2.EndDate = ReadFullDate();
-
-    cout << "Overlap Days Count is: " << CountOverLapDays(Period1, Period2) << endl;
     
 
     system("pause>0");
