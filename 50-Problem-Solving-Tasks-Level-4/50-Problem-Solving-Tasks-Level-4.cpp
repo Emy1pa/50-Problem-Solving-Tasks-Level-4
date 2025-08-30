af11a034386902be32abb2bb6e1ab9438ac779b2
Emy1pa <souaouti.iman@gmail.com>
2025-08-30 16:40:42 +0100

    Solve Problem 58: implement period overlap check using date comparison functions to determine if two date ranges intersect or not

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -37,6 +37,10 @@ struct sDate {
     short Day;
 };
 
+struct stPeriod {
+    sDate StartDate;
+    sDate EndDate;
+};
 sDate ReadFullDate() {
     sDate Date;
     Date.Day = ReadDay();
@@ -44,14 +48,8 @@ sDate ReadFullDate() {
     Date.Year = ReadYear();
     return Date;
 }
-
-bool IsLeapYear(short Year)
+bool IsDate1BeforeDate2(sDate Date1, sDate Date2)
 {
-    return (Year % 4 == 0 && Year % 100 != 0) ||
-        (Year % 400 == 0);
-}
-
-bool IsDate1BeforeDate2(sDate Date1, sDate Date2){
     return (Date1.Year < Date2.Year) ? true :
         ((Date1.Year == Date2.Year) ?
             (Date1.Month < Date2.Month ? true :
@@ -59,43 +57,76 @@ bool IsDate1BeforeDate2(sDate Date1, sDate Date2){
                     Date1.Day < Date2.Day : false))
             : false);
 }
-
-bool IsDate1EqualDate2(sDate Date1, sDate Date2){
+bool IsDate1EqualDate2(sDate Date1, sDate Date2)
+{
     return (Date1.Year == Date2.Year) ? ((
         Date1.Month == Date2.Month) ?
         ((Date1.Day == Date2.Day) ? true :
             false) : false) : false;
 }
-
-bool IsDate1AfterDate2(sDate Date1, sDate Date2) {
-    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2));
+bool IsDate1AfterDate2(sDate Date1, sDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2)
+        && !IsDate1EqualDate2(Date1, Date2));
 }
 
-enDateComparison DateComparison(sDate Date1, sDate Date2) {
+enum enDateCompare { Before = -1, Equal = 0, After = 1 };
+
+enDateCompare CompareDates(sDate Date1, sDate
+    Date2)
+{
     if (IsDate1BeforeDate2(Date1, Date2))
-        return enDateComparison::enBefore;
-    else if (IsDate1AfterDate2(Date1, Date2))
-        return enDateComparison::enAfter;
-    else
-        return enDateComparison::enEqual;
+        return enDateCompare::Before;
+    if (IsDate1EqualDate2(Date1, Date2))
+        return enDateCompare::Equal;
+    return enDateCompare::After;
 }
+bool checkForOverlapPeriod(stPeriod Period1, stPeriod Period2){
+    if (
+        CompareDates(Period2.EndDate,
+            Period1.StartDate) == enDateCompare::Before
+        ||
+        CompareDates(Period2.StartDate,
+            Period1.EndDate) == enDateCompare::After
+        )
+        return false;
+    else
+        return true;
+}
+
+
+
+
 
 int main()
 {
 
     
-    sDate Date1, Date2;
-    cout << "Enter Date1: \n";
-    Date1 = ReadFullDate();
-    cout << endl;
+    stPeriod Period1, Period2;
+   
 
-    cout << "Enter Date2: \n";
-    Date2 = ReadFullDate();
+    cout << "Enter Period 1: \n";
+    cout << "Enter Start Date: \n\n";
 
-    
+    Period1.StartDate = ReadFullDate();
 
-    cout << "\nCompare Result = " << DateComparison(Date1, Date2) << endl;
+    cout << "\nEnter End Date: \n\n";
 
+    Period1.EndDate = ReadFullDate();
+
+    cout << "\nEnter Period 2: \n";
+    cout << "Enter Start Date: \n\n";
+
+    Period2.StartDate = ReadFullDate();
+
+    cout << "\nEnter End Date: \n\n";
+
+    Period2.EndDate = ReadFullDate();
+
+    if (checkForOverlapPeriod(Period1, Period2))
+        cout << "\nYes, Periods Overlap\n";
+    else
+        cout << "\nNo, Periods do not Overlap\n";
     system("pause>0");
 
 }
