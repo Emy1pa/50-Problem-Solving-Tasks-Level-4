6ca823b631c1c0bd42febb51b5f8e50ab2dab97b
Emy1pa <souaouti.iman@gmail.com>
2025-08-30 21:24:54 +0100

    Solve Problem 60: implement function to check if a given date falls within a specified period range inclusively

--- 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
+++ 50-Problem-Solving-Tasks-Level-4/50-Problem-Solving-Tasks-Level-4.cpp
@@ -54,6 +54,21 @@ bool IsDate1BeforeDate2(sDate Date1, sDate Date2)
             : false);
 }
 
+bool IsDate1EqualDate2(sDate Date1, sDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((
+        Date1.Month == Date2.Month) ?
+        ((Date1.Day == Date2.Day) ? true :
+            false) : false) : false;
+}
+
+bool IsDate1AfterDate2(sDate Date1, sDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2)
+        && !IsDate1EqualDate2(Date1, Date2));
+}
+
+
 bool IsLeapYear(short Year)
 {
     return (Year % 4 == 0 && Year % 100 != 0) ||
@@ -104,25 +119,63 @@ sDate IncreaseDateByOneDay(sDate Date)
     return Date;
 }
 
-short CalculatePeriodLength(stPeriod Period, bool IncludeEndDay= false){
-    int Days = 0;
+//short CalculatePeriodLength(stPeriod Period, bool IncludeEndDay= false){
+//    int Days = 0;
+//
+//    while (IsDate1BeforeDate2(Period.StartDate, Period.EndDate)) {
+//        Days++;
+//        Period.StartDate = IncreaseDateByOneDay(Period.StartDate);
+//    }
+//    return IncludeEndDay ? ++Days : Days;
+//}
 
-    while (IsDate1BeforeDate2(Period.StartDate, Period.EndDate)) {
-        Days++;
-        Period.StartDate = IncreaseDateByOneDay(Period.StartDate);
-    }
-    return IncludeEndDay ? ++Days : Days;
+void SwapDates(sDate& Date1, sDate& Date2)
+{
+    sDate TempDate;
+    TempDate.Year = Date1.Year;
+    TempDate.Month = Date1.Month;
+    TempDate.Day = Date1.Day;
+    Date1.Year = Date2.Year;
+    Date1.Month = Date2.Month;
+    Date1.Day = Date2.Day;
+    Date2.Year = TempDate.Year;
+    Date2.Month = TempDate.Month;
+    Date2.Day = TempDate.Day;
 }
 
+int GetDifferenceInDays(sDate Date1, sDate Date2, bool IncludeEndDay = false)
+{
+    int Days = 0;
+    short SwapFlagValue = 1;
+    if (!IsDate1BeforeDate2(Date1, Date2))
+    {
+        SwapDates(Date1, Date2);
+        SwapFlagValue = -1;
+    }
+    while (IsDate1BeforeDate2(Date1, Date2))
+    {
+        Days++;
+        Date1 = IncreaseDateByOneDay(Date1);
+    }
+    return IncludeEndDay ? ++Days * SwapFlagValue : Days * SwapFlagValue;
+}
 
+int CalculatePeriodLength(stPeriod Period, bool IncludeEndDate = false){
+    return GetDifferenceInDays(Period.StartDate, Period.EndDate, IncludeEndDate);
+}
+
+bool CheckIfDateWithinPeriod(stPeriod Period, sDate Date){
+    return (!IsDate1BeforeDate2(Date, Period.StartDate)&& !IsDate1AfterDate2(Date, Period.EndDate));
+}
 
 int main()
 {
 
     
     stPeriod Period1;
-   
 
+    sDate Date;
+   
     cout << "Enter Period 1: \n";
     cout << "Enter Start Date: \n\n";
 
@@ -132,10 +185,15 @@ int main()
 
     Period1.EndDate = ReadFullDate();
 
-    cout << "\n\nPeriod Length is: " << CalculatePeriodLength(Period1) << endl;
-    cout << "Period Length (Including End Date) is: " << CalculatePeriodLength(Period1, true) << endl;
+    cout << "\n\nEnter Date to check: \n\n";
 
-    
+    Date = ReadFullDate();
+
+    if (CheckIfDateWithinPeriod(Period1, Date))
+        cout << "\nYes, Date is within period\n";
+    else
+        cout << "\nNon, Date is not within period \n";
+
     system("pause>0");
 
 }
